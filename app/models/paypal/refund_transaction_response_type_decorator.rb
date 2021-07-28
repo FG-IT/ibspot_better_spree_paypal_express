e414494d8b8fca7c28ba6c8465c76aec3ea150cc
JX <johnnyxiang2017@gmail.com>
2021-07-27 18:05:37 -0700

    bugs & cancel function

---
+++ app/models/paypal/refund_transaction_response_type_decorator.rb
@@ -0,0 +1,18 @@
+module PayPal
+  module RefundTransactionResponseTypeDecorator
+
+    def authorization
+      self.to_hash[:RefundTransactionID]
+    end
+
+    def params
+      {:message => self.to_hash[:LongMessage]}
+    end
+
+    def message
+      self.to_hash[:ShortMessage]
+    end
+  end
+end
+
+::PayPal::SDK::Merchant::DataTypes::RefundTransactionResponseType.prepend(PayPal::RefundTransactionResponseTypeDecorator)
